0c7afac30fdbea51bdca462db242788e03c63a86
EWA CHWEDCZUK <echwedczuk0@gmail.com>
2022-08-22 10:17:21 +0200

    add pokemons to pokedex

--- src/pages/AddToPokedex.tsx
+++ src/pages/AddToPokedex.tsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import Layout from '../components/Layout';
 import Nav from '../components/Nav';
 import getColorByPokemonType from '../helpers/getColorByPokemonType';
-import { usePokemonContext } from '../lib/context';
+import { Pokemon, usePokemonContext } from '../lib/context';
 import styles from './AddToPokedex.module.scss';
 
 interface LocationState {
@@ -12,88 +12,104 @@ interface LocationState {
 
 const AddToPokedex = () => {
   const [enteredPokemonName, setEnteredPokemonName] = useState('');
+  const [chosenPokemon, setChosenPokemon] = useState<Pokemon>();
+
   const navigation = useNavigate();
+  const locationState = useLocation().state as LocationState;
+
+  const { addPokemonToPokedex, allPokemons, pokedex } = usePokemonContext();
 
   const pokemonNameHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
     setEnteredPokemonName(event.target.value);
   };
 
+  useEffect(() => {
+    if (locationState === null || locationState === undefined) {
+      return;
+    }
+    if (
+      locationState.enteredSearch !== undefined &&
+      locationState.enteredSearch !== ''
+    ) {
+      const pokemon = allPokemons.find(
+        (pokemon) => locationState.enteredSearch === pokemon.name
+      );
+      setChosenPokemon(pokemon);
+    }
+  }, [allPokemons, locationState]);
+
   const onSaveSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+
     if (enteredPokemonName.trim() === '') {
       return;
     }
+    if (chosenPokemon === undefined) return;
+    addPokemonToPokedex([chosenPokemon, ...pokedex]);
     navigation('/pokedex');
   };
 
-  const { allPokemons } = usePokemonContext();
-  const state = useLocation().state as LocationState;
-  if (state === null || state === undefined) {
-    return <p>Error</p>;
-  }
-  if (state.enteredSearch !== undefined && state.enteredSearch !== '') {
-    const pokemon = allPokemons.find(
-      (pokemon) => state.enteredSearch === pokemon.name
-    );
-    console.log(enteredPokemonName);
-
-    if (pokemon !== undefined)
-      return (
-        <Layout>
-          <Nav />
-          <div
-            className={styles.container}
-            style={{
-              backgroundColor: getColorByPokemonType(pokemon.types[0].name),
-            }}
-          >
-            <div className={styles.title}>
-              <h1>{pokemon.name}</h1>
-              <h3>{pokemon.nat_dex_num}</h3>
-            </div>
-            <div className={styles.saveContainer}>
-              <div className={styles.types}>
-                <img src={pokemon.sprites.front_default} alt={pokemon.name} />
-                <div className={styles.typeInfo}>
-                  {pokemon.types.map((type) => (
-                    <p
-                      style={{
-                        backgroundColor: getColorByPokemonType(type.name),
-                      }}
-                      key={pokemon.nat_dex_num}
-                    >
-                      {type.name}
-                    </p>
-                  ))}
-                </div>
-              </div>
-              <div className={styles.addToPokedex}>
-                <h3>Add Name</h3>
-                <form className={styles.inputContainer} onSubmit={onSaveSubmit}>
-                  <input
-                    type="text"
-                    name=""
-                    id=""
-                    placeholder="Name"
-                    value={enteredPokemonName}
-                    onChange={pokemonNameHandler}
-                  />
-                  <button
+  if (chosenPokemon !== undefined) {
+    return (
+      <Layout>
+        <Nav />
+        <div
+          className={styles.container}
+          style={{
+            backgroundColor: getColorByPokemonType(chosenPokemon.types[0].name),
+          }}
+        >
+          <div className={styles.title}>
+            <h1>{chosenPokemon.name}</h1>
+            <h3>{chosenPokemon.nat_dex_num}</h3>
+          </div>
+          <div className={styles.saveContainer}>
+            <div className={styles.types}>
+              <img
+                src={chosenPokemon.sprites.front_default}
+                alt={chosenPokemon.name}
+              />
+              <div className={styles.typeInfo}>
+                {chosenPokemon.types.map((type) => (
+                  <p
                     style={{
-                      backgroundColor: getColorByPokemonType(
-                        pokemon.types[0].name
-                      ),
+                      backgroundColor: getColorByPokemonType(type.name),
                     }}
+                    key={chosenPokemon.nat_dex_num}
                   >
-                    Add to Pokedex
-                  </button>
-                </form>
+                    {type.name}
+                  </p>
+                ))}
               </div>
+            </div>
+            <div className={styles.addToPokedex}>
+              <h3>Add Name</h3>
+              <form className={styles.inputContainer} onSubmit={onSaveSubmit}>
+                <input
+                  type="text"
+                  name=""
+                  id=""
+                  placeholder="Name"
+                  value={enteredPokemonName}
+                  onChange={pokemonNameHandler}
+                />
+                <button
+                  style={{
+                    backgroundColor: getColorByPokemonType(
+                      chosenPokemon.types[0].name
+                    ),
+                  }}
+                >
+                  Add to Pokedex
+                </button>
+              </form>
             </div>
           </div>
-        </Layout>
-      );
+        </div>
+      </Layout>
+    );
   }
+
   return <div>Hello world</div>;
 };
 
--- src/pages/MyPokedex.tsx
+++ src/pages/MyPokedex.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Layout from '../components/Layout';
 import Nav from '../components/Nav';
+import OnePokemonList from '../components/OnePokemonList';
 import { usePokemonContext } from '../lib/context';
 import styles from './AllPokemons.module.scss';
 
@@ -13,7 +15,17 @@ const MyPokedex = () => {
       <div className={styles.allContainer}>
         <div className={styles.insideContainer}>
           <h2>My Pokedex</h2>
-          <div className={styles.itemsContainer}></div>
+          <div className={styles.itemsContainer}>
+            {pokedex.map((pokemon) => (
+              <Link
+                className={styles.link}
+                to="/pokemoninfo"
+                state={{ enteredSearch: pokemon.name }}
+              >
+                <OnePokemonList pokemon={pokemon} />
+              </Link>
+            ))}
+          </div>
         </div>
       </div>
     </Layout>
